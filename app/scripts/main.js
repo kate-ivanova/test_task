d0e8b340ce0afc9621b7370deebeb81d368d8ab9
Ekaterina Ivanova <ekaterina.ivanova@saltpepper.ru>
2015-08-20 16:16:39 +0300

    Add backbone

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+_.extend($('input'), Backbone.Events);
+$('input').on('input', function(){
+  $('#' + $(this).attr('id') + '-val').html($(this).val());
+  switch($(this).attr('id')){
+    case 'birth-day':
+    case 'birth-month':
+      $('#' + $(this).attr('id') + '-val').html($('#' + $(this).attr('id') + '-val').html() + '.')
+      break;
+  }
+});
+$('select').on('change', function(){
+  $('#' + $(this).attr('id') + '-val').html($(this).val());
+});
