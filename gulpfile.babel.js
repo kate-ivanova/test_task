2126df93cbdc229bdd5c263f304fa6c65c38c557
Ekaterina Ivanova <ekaterina.ivanova@saltpepper.ru>
2015-09-17 11:05:55 +0300

    Fix bugs

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -179,7 +179,7 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app/layouts'));
 });
 
-gulp.task('build', ['clean', 'lint', 'html', 'images', 'fonts', 'extras'], () => {
+gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
